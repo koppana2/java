bb42b53182eec4fc9b75d3ad9018fd76319804ba
koppana2 <138375424+koppana2@users.noreply.github.com>
2023-07-03 12:20:28 +0530

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -1,13 +1,43 @@
-package abstraction;
+package classes_Objects;
 
 public class MainClass {
 
 	public static void main(String[] args) {
-		Circle c1 = new Circle(2.3f, "0.##");
-		Rectangle r1 = new Rectangle(2, 4.1f, "0.##");
-		System.out.println(c1);
-		System.out.println(r1);
+		// ClassName ObjectName= new ClassName();
+		Mobile samsung = new Mobile();
 
+		// Access class Members : objectname.member
+		samsung.RAM = 8;
+		samsung.ROM = 64;
+		samsung.OS = "Android 11";
+		samsung.processor = "Snapdragon 720";
+
+		System.out.println("RAM : " + samsung.RAM);
+		System.out.println("ROM : " + samsung.ROM);
+		System.out.println("OS : " + samsung.OS);
+		System.out.println("processor : " + samsung.processor);
+
+		samsung.communication();
+		samsung.gaming();
+		samsung.browsing();
+
+		System.out.println("--------------------------------------------------");
+
+		Mobile iphone = new Mobile();
+
+		iphone.RAM = 4;
+		iphone.ROM = 256;
+		iphone.OS = "ios";
+		iphone.processor = "Bionic";
+
+		System.out.println("RAM : " + iphone.RAM);
+		System.out.println("ROM : " + iphone.ROM);
+		System.out.println("OS : " + iphone.OS);
+		System.out.println("processor : " + iphone.processor);
+
+		iphone.communication();
+		iphone.gaming();
+		iphone.browsing();
 	}
 
 }
---
+++ Mobile.java
@@ -0,0 +1,19 @@
+package classes_Objects;
+
+public class Mobile {
+
+	int RAM, ROM;
+	String OS, processor;
+
+	public void communication() {
+		System.out.println("Texts,Calls");
+	}
+
+	public void gaming() {
+		System.out.println("video games");
+	}
+
+	public void browsing() {
+		System.out.println("Social media,daily updates");
+	}
+}
